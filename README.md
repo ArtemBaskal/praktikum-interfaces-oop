a362c42ca12f909c495ca55fd304e0debf84d8a8
Artem Baskal <a.baskal@adguard.com>
2020-11-26 20:20:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Каноническая работа четверый спринт, верстка
+
+## :thinking: Описание
+
+### [Pages](https://pcr-web.gitlab.io/canonical_ru-en_sprint7/)
+
+Проекты создаются на базе тренажера без учета материала, подаваемого наставниками.
+
+| Тема        | Ссылка |
+| ------------- | ------------- |
+| Flexbox | [Первый урок](https://praktikum.yandex.ru/learn/web/courses/dbf98e55-0f76-444b-850c-4538708ad571/sprints/1425/topics/e082eec5-aa3f-4fa2-81e5-b1a929df03f2/lessons/52a11a7d-22d5-459e-af4d-3eae8b3e6f94/) |
+| БЭМ, Семантика, Псевдоселекторы и элементы | [Первый урок](https://praktikum.yandex.ru/learn/web/courses/dbf98e55-0f76-444b-850c-4538708ad571/sprints/1425/topics/1d37621d-8cab-4d84-b2e4-77b76e08c168/lessons/c85c9058-6ee3-41a9-8ede-da1f381f48ab/) |
+| Позиционирование элементов, Z-Index | [Первый урок](https://praktikum.yandex.ru/learn/web/courses/dbf98e55-0f76-444b-850c-4538708ad571/sprints/1425/topics/65007471-e104-4987-9f04-7c8fc6966bde/lessons/0e4896a3-3f2e-4b03-b469-131f2a9efa19/) |
+| Структура и пути к файлам | [Первый урок](https://praktikum.yandex.ru/learn/web/courses/dbf98e55-0f76-444b-850c-4538708ad571/sprints/1425/topics/a0dc3302-cc77-4261-b401-0625b9e60b02/lessons/ebf9f4c3-ce4e-4170-a139-e579bbfff863/) |
+
+*Выбранные подходы могут показаться не оптимальными. Однако соответствуют решению текущей задачи.
+Некоторые фрагменты кода содержат комментарии по улучшению и оптимизации. А также альтернативные варианты решения.*
+
+## :no_entry: Внесение изменений
+
+### [Issues](https://gitlab.com/pcr-web/canonical_ru-en_sprint7/-/issues)
+
+### Если есть что-то, что хотите запушить в проект:
+* Pull-request are welcome. Делайте сайд-бранч формата `type/name`, f.e `bugfix/header`
+* Пишите короткие, содержательные коммиты. Без атомарщины. Пользуйтесь [этим](https://www.conventionalcommits.org/ru/v1.0.0-beta.4/)
---
+++ public/scripts/FormValidator.js
@@ -0,0 +1,72 @@
+class FormValidator {
+  constructor(config, popupElement) {
+    this._inputSelector = config.inputSelector;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+
+    this._element = popupElement;
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._element.querySelector(`#${inputElement.id}-error`);
+
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._element.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.textContent = '';
+    errorElement.classList.remove(this._errorClass);
+  }
+
+  _toggleButtonState() {
+    if (this._getInvalidInput) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _getInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(this._element.querySelectorAll(this._inputSelector));
+    this._buttonElement = this._element.querySelector(this._submitButtonSelector);
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._element.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+
+    this._setEventListeners();
+  }
+}
+
+export default FormValidator;
